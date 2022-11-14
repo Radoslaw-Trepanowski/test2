6b438bccb9c8b683c6c3b90cafecfb09cb98c884
Radoslaw-Trepanowski <105867160+Radoslaw-Trepanowski@users.noreply.github.com>
2022-11-14 23:53:59 +0100

    Update qualt.js

--- qualt.js
+++ qualt.js
@@ -1,39 +1,40 @@
-define(['pipAPI', 'https://radoslaw-trepanowski.github.io/test2/test.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
 		category1 : {
-			name : 'Black people', //Will appear in the data.
+			name : Kariera', //Will appear in the data.
 			title : {
-				media : {word : 'Black people'}, //Name of the category presented in the task.
+				media : {word : Kariera}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'black1.jpg'}, 
-    			{image : 'black2.jpg'}, 
-    			{image : 'black3.jpg'}, 
-    			{image : 'black4.jpg'}, 
-    			{image : 'black5.jpg'}, 
-    			{image : 'black6.jpg'}
+    		    {word : 'Korporacja'},
+				{word : 'Pensja'},
+				{word : 'Biuro'},
+				{word : 'Zawodowy'},
+				{word : 'Zarzadzanie'},
+				{word : 'Biznes'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'White people', //Will appear in the data.
+			name : 'Rodzina', //Will appear in the data.
 			title : {
-				media : {word : 'White people'}, //Name of the category presented in the task.
+				media : {word : Rodzina}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'yf1.jpg'}, 
-    			{image : 'yf4.jpg'}, 
-    			{image : 'yf5.jpg'}, 
-    			{image : 'ym2.jpg'}, 
-    			{image : 'ym3.jpg'}, 
-    			{image : 'ym5.jpg'}			], 
+    		{word: 'slub'},
+				{word: 'Malzenstwo'},
+				{word: 'Rodzice'},
+				{word: 'Krewni'},
+				{word: 'Rodzina'},
+				{word: 'Dom'}, 
+				{word: 'Dzieci'} 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
@@ -43,5 +44,3 @@ define(['pipAPI', 'https://radoslaw-trepanowski.github.io/test2/test.js'], funct
 		} 
 	});
 });
-
-
